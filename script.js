02479faa130bd6e0def871c7a4808dfb671610d3
kabdomora <114969831+kabdomora@users.noreply.github.com>
2022-11-28 21:44:13 -0800

    bug fixes

--- script.js
+++ script.js
@@ -40,10 +40,7 @@ form.addEventListener('submit', (e) => {
         getMovies(API_URL);
     }
 
-    displaySearch.style.visibility = 'visible';
     search.value = '';
-
-    
 
 })
 
@@ -80,6 +77,7 @@ function moviePoster() {
 `;
 
 
+
 /* <div class="overview"> */
 //        <h3 id=${id}+${poster_path}+${vote_average}+${ovID}+${movieID}>Overview: 
 //        <button id="movie-button">Go To Movie 👉
@@ -97,6 +95,7 @@ function moviePoster() {
     // poster.appendChild(imgPoster);
 
 }
+
 
 function streamHere() {
     const options = {
@@ -149,13 +148,6 @@ function streamHere() {
 
 
                 stream.appendChild(streamOptions); 
-                
-                
-
-                console.log(logo);
-                console.log(name);
-                console.log(link);
-
                 }
             })
 
@@ -164,16 +156,6 @@ function streamHere() {
         .catch(err => console.error(err));
 
 }
-
-
-
-// function removeDup(streamOptions) {
-//     let result = []
-//     streamOptions.forEach((item, index) => { if (result.indexOf(item) === -1) result.push(item) });
-//     return result;
-//     }
-
-// removeDup();
 
 
 function movieDetails() {
@@ -229,6 +211,52 @@ function movieTrailer() {
 }
 
 
+function similarRecommendations() {
+    var selected = localStorage.getItem('movieID');
+    let pullSimilarTitles = BASE_URL.concat('/movie/', selected, '/similar?', API_KEY, '&language=en-US');
+    var similar = document.getElementById('similar');
+
+    fetch(pullSimilarTitles)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        similar.innerHTML = '';
+        console.log(data);
+
+        var recommendedHeader = document.createElement('h3');
+        recommendedHeader.setAttribute('class', "flex flex-row px-20 text-xl font-sans font-extrabold py-5 underline");
+        recommendedHeader.innerHTML = 'Similar Recommendations';
+        similar.appendChild(recommendedHeader);
+
+        data.results.forEach(movie => {
+            const {title, poster_path, vote_average, overview, id} = movie;
+            const similarEl = document.createElement('div');
+            similarEl.classList.add('movieSimilar');
+            similarEl.setAttribute('id', id);
+            similarEl.innerHTML = `
+            <img src="${IMG_URL+poster_path}" alt="${title}">
+            <div class="movie-info">
+               <h3>${title}</h3>
+               <span class="${getColor(vote_average)}">${vote_average}</span>
+             </div>
+             <div class="overview">
+               <h3 id=${id}-${poster_path}>Overview: 
+               <button id="movie-button">Go To Movie 👉
+               <span id='movieID' class='text-xs invisible'>${id}</span>
+               </button>
+               </h3>
+               ${overview}
+           </div>
+       
+       `;
+        similar.appendChild(similarEl);
+
+
+        })
+    });
+    similar.setAttribute('class', 'flex flex-row relative rounded-xl overflow-auto');
+}
 
 
 function showMovies(data) {
@@ -264,31 +292,33 @@ function showMovies(data) {
         
     })
 
-    var movieTile = document.querySelector('#displaySearch');
-    
-    movieTile.addEventListener('click', function(event) {
-        var element = event.target;
 
-        if (element.matches('button') === true) {
-            var index = element.parentElement.getAttribute('id');
-            movie.splice(index, 1);
-            console.log(index);
-            var splitIDs = index.split("+");
-            localStorage.setItem('movieID', splitIDs[0]);  
-            localStorage.setItem('posterPath', splitIDs[1]);
-            localStorage.setItem('movieRating', splitIDs[2]);
-            localStorage.setItem('overview', splitIDs[3]);
-            localStorage.setItem('movieTitle', splitIDs[4]);
-
-            moviePoster();
-            movieTrailer();
-            streamHere();
-            movieDetails();
-            
-        }
-    });
 
 }
+
+var movieTile = document.querySelector('#displaySearch');
+    
+movieTile.addEventListener('click', function(event) {
+    var element = event.target;
+
+    if (element.matches('button') === true) {
+        var index = element.parentElement.getAttribute('id');
+        movie.splice(index, 1);
+        console.log(index);
+        var splitIDs = index.split("+");
+        localStorage.setItem('movieID', splitIDs[0]);  
+        localStorage.setItem('posterPath', splitIDs[1]);
+        localStorage.setItem('movieRating', splitIDs[2]);
+        localStorage.setItem('overview', splitIDs[3]);
+        localStorage.setItem('movieTitle', splitIDs[4]);
+
+        moviePoster();
+        movieTrailer();
+        streamHere();
+        movieDetails();
+        
+    }
+});
 
 
 function getColor(vote) {
@@ -304,76 +334,3 @@ function getColor(vote) {
 
 
 
-
-
-
-
-// request API for the videos associated with a movie: https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US 
-// The trailer you can watch this way:
-
-// Trailer URL
-
-// When you make an API Request requesting Videos,
-// in the response you will receive something like this:
-
-// videos  
-// results 
-// 0   
-// iso_639_1   "en"
-// iso_3166_1  "US"
-// name    "Fight Club - Theatrical Trailer Remastered in HD"
-// key "6JnN1DmbqoU"
-// site    "YouTube"
-// size    1080
-// type    "Trailer"
-// official    false
-// published_at    "2015-02-26T03:19:25.000Z"
-// id  "5e382d1b4ca676001453826d"
-// Just add the Key in the respective URL:
-
-// YouTube: https://www.youtube.com/watch?v=  
-// Vimeo: https://vimeo.com/  
-// For example:
-// Youtube: https://www.youtube.com/watch?v=6JnN1DmbqoU
-// Youtube: https://www.youtube.com/watch?v=hzfvYSIIgyc
-// Vimeo: https://vimeo.com/282875052
-
-// 
-//  $(function() {
-//    var recommendation_account_check = false;
-//    var waypoint = new Waypoint({
-//      element: document.getElementById('recommendation_waypoint'),
-//      handler: function(direction) {
-//        if (direction == 'down' && !recommendation_account_check) {
-//          recommendation_account_check = true;
-//          attachLoggedInAccountTooltips(["544fe82f0e0a26134c00241d", "53d5f94f0e0a26284a006b91", "53d5f8190e0a26283e006d52", "4f7a4487760ee35b970086f0", "5616911fc3a368681701f711", "537d787ac3a3680598000870", "54a9ab849251414d5b005173", "4bc8c584017a3c122d086e4a", "56c61bc8925141245400358e", "53d54a3cc3a3686b910042f7", "53d5f75d0e0a26283b0069bf", "544fec3c0e0a2601d8002462", "4f8fd90c760ee347f8005dc5", "4e9804c05e73d65301002283", "4ff4d0aa19c2954d4f00237f", "584bd017c3a368397200969e", "4f7dd91119c29512540037b4", "4bc8ac2e017a3c122d04eeb4", "4bc896cf017a3c122d016dfd", "4bc896bb017a3c122d016b31", "4bc8abee017a3c122d04e733"], 'deebo18', '6375b9de66a7c300d552b43a', 'Add to your favorite list', 'Add to your watchlist', 'Remove from your favorite list', 'Remove from your watchlist');
-//          enableLoggedInAccountTooltipActions('movie', 'deebo18', 'Add to your favorite list', 'Add to your watchlist', 'Remove from your favorite list', 'Remove from your watchlist');
-//        }
-//      },
-//      offset: '100%'
-//    });
-//});
-
-
-// var recommendation_waypoint_check = false;
-// new Waypoint({
-//  element: document.getElementById('recommendation_waypoint'),
-//  handler: function(direction) {
-//    if (direction == 'down' && !recommendation_waypoint_check) {
-//      $.ajax({
-//        url: kendo.format('/{0}/{1}/remote/recommendations', 'movie', '299536-avengers-infinity-war'),
-//        type: 'GET',
-//        data: {
-//          version: 1,
-//          translate: false
-//        }
-//      }).done(function(response) {
-//        recommendation_waypoint_check = true;
-//        $('#recommendation_waypoint').html(response).hide().fadeIn(1000);
-//        initializeScroller(false, '#recommendation_scroller');
-//        observer.observe();
-//      });
-//    }
-//  },
-//  offset: '100%'
-// });
